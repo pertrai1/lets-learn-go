a3b486c17ab213152e900c0821fba81cecdb689a
Rob Simpson <rsimpson2@me.com>
2022-09-06 01:07:25 -0400

    add routes for snippets

--- main.go
+++ main.go
@@ -9,9 +9,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+// Add a snippetView handler func
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display specific snippet..."))
+}
+
+// Add a snippCreate handler func
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
